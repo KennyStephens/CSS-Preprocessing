c0eecfba74941f2ee97c959e7797cff63cf0ef86
KennyStephens <kennethcstephens@gmail.com>
2017-12-13 17:23:16 -0700

    CSS Preprocessing

---
+++ js/main.js
@@ -0,0 +1,12 @@
+TweenMax.from(".header", 1.25, {opacity:0, scale: 0, ease:Back.easeIn});
+
+TweenMax.to("body", 1, {backgroundColor:"#eaeaea"});
+
+TweenMax.from(".orisa", 1, {opacity: 0, y:200});
+
+TweenMax.from(".reaper", 1, {opacity: 0, y:-200});
+
+TweenMax.from(".reinhardt", 1, {opacity: 0, x:200});
+
+TweenMax.from(".zenyatta", 1, {opacity: 0, x:-200});
+
